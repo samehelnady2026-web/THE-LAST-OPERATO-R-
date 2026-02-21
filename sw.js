07fa922a2426bf5eb517ac57b5059204f837f106
samehelnady2026-web <samehelnady2026@gmail.com>
2026-02-21 10:24:52 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'operator-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/icon.png',
+  '/manifest.json'
+];
+
+// تثبيت الملفات في ذاكرة الموبايل
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// تشغيل اللعبة من الذاكرة لو مفيش إنترنت
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
